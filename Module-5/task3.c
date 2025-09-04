54a5682ac517d849028f875fdd727edd15c8f33a
Yuriy Perezhogin <wolfviking@yandex.ru>
2025-09-04 15:57:34 +0500

    task3

---
+++ Module-5/task3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    float x = 2.4f;
+    float y = 3.7f;
+    float b1 = x + y;
+    int a = (int)(72.45 + 84.32);
+    float b = 44.32 + 51.54;
+    float b2 = (float)53 + 2.54;
+    float b3 = (float)12 + 22;
+    int a2 = (int)64.2154;
+    float a3 = (float)42;
+    printf("%.1f\n", x);
+    printf("%.1f\n", y);
+    printf("%.1f\n", b1);
+    printf("%d\n", a);
+    printf("%.2f\n", b);
+    printf("%.2f\n", b2);
+    printf("%.2f\n", b3);
+    printf("%d\n", a2);
+    printf("%.2f\n", a3);
+
+    return 0;
+}
