efa0a894e254de3b465f1361ae4441217ac7ac5b
Yuriy Perezhogin <wolfviking@yandex.ru>
2025-09-05 13:31:18 +0500

    add task4

---
+++ Module-6/task4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    int day;
+    printf("Введите число от 1 до 7: ");
+    scanf("%d", &day);
+
+    switch(day) {
+        case 1: 
+            printf("Сегодня понедельник\n");
+            break;
+        case 2:
+            printf("Сегодня вторник\n");
+            break;
+        case 3:
+            printf("Сегодня среда\n");
+            break;
+        case 4:
+            printf("Сегодня четверг\n");
+            break;
+        case 5:
+            printf("Сегодня пятница\n");
+            break;
+        case 6:
+            printf("Сегодня суббота");
+            break;
+        case 7:
+            printf("Сегодня воскресенье");
+            break;
+        default:
+            printf("Error!\n");
+            printf("Вы ввели неверное число!\n");    
+    }
+    return 0;
+}
